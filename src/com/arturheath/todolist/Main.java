57765a540f3f03dd496d36bb9a2dc5a002f06e15
arturheath <arshamrin@gmail.com>
2018-10-19 18:04:41 +0800

    Initial commit

---
+++ src/com/arturheath/todolist/Main.java
@@ -0,0 +1,45 @@
+package com.arturheath.todolist;
+
+import com.arturheath.todolist.datamodel.TodoData;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+        primaryStage.setTitle("ToDo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception{
+
+        try{
+            TodoData.getInstance().storeToDoItems();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+
+        try{
+            TodoData.getInstance().loadToDoItems();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
